85510637299a2a0f4fc2ecf55d8183ce417a8a46
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2023-11-27 10:52:51 +0700

    sync

--- app/src/main/java/com/example/eabsen/API/URI.java
+++ app/src/main/java/com/example/eabsen/API/URI.java
@@ -1,5 +1,5 @@
 package com.example.eabsen.API;
 
 public class URI {
-    public  static  String url= "http://192.168.43.145:8000/api/";
+    public  static  String url= "http://192.168.253.185:4500/api/";
 }
